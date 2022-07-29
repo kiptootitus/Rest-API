8366c9a9b71a73879b491386e0696fc3ce2ff974
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2022-07-29 13:37:10 +0300

    error handlers

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const productRoutes=require('./routes/products');	
 const orderRoutes=require('./routes/orders');
+
+app.use(morgan('dev')); 
+
+//handle routes
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = app;
