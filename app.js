f7a1a38212396d429d56ddbc750564a2a96e9537
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2022-07-29 12:57:42 +0300

    GET, POST, PATCH , DELETE routes

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const productRoutes=require('./routes/products');	
+const orderRoutes=require('./routes/orders');
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+module.exports = app;
